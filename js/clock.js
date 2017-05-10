adbfc2af7e11309578906442b0545f48d770f82b
Krzysztof Glinka <krzysztof.m.glinka@gmail.com>
2017-05-10 22:47:32 +0100

    clock.js

---
+++ js/clock.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  var hourHand = document.querySelector('.hour');
+  var minsHand = document.querySelector('.minutes');
+  var secondHand = document.querySelector('.seconds');
+
+function setDate() {
+
+  var now = new Date();
+  var seconds = now.getSeconds();
+  var secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  var mins = now.getMinutes();
+  var minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  var hour = now.getHours();
+  var hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+setInterval(setDate, 1000);
+setDate();
+})
