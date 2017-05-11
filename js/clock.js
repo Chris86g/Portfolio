e43da7e0bdb0af21387369def84b91f13fceedcb
Krzysztof Glinka <krzysztof.m.glinka@gmail.com>
2017-05-11 23:20:45 +0100

    icon, clock

--- js/clock.js
+++ js/clock.js
@@ -8,13 +8,13 @@ function setDate() {
 
   var now = new Date();
   var seconds = now.getSeconds();
-  var secondsDegrees = ((seconds / 60) * 360) + 90;
+  var secondsDegrees = ((seconds / 60) * 360);
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   var mins = now.getMinutes();
-  var minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  var minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6);
   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
   var hour = now.getHours();
-  var hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  var hourDegrees = ((hour / 12) * 360) + ((mins/60)*30);
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 setInterval(setDate, 1000);
